ca4b841b3e669c1566a1562fc72b3049fd0b4011
Dmitry Polyanitsa <coffeecup.winner@gmail.com>
2021-07-10 11:57:42 +0200

    Runner: parallelize

--- src/runner.rs
+++ src/runner.rs
@@ -1,3 +1,5 @@
+use rayon::prelude::*;
+
 use crate::common::*;
 use crate::problem::{Pose, Problem};
 use crate::solver::SOLVERS;
@@ -13,37 +15,52 @@ pub fn run(
     };
     solver_names.sort();
     let count = problems_path.read_dir()?.count();
-    Ok(for i in 1..=count {
-        let problem = Problem::from_json(&std::fs::read(
-            problems_path.join(format!("{}.problem", i)),
-        )?)?;
-        let solution_path = solutions_base_path.join(format!("{}.solution", i));
-        let mut best_dislikes = if solution_path.exists() {
-            problem.dislikes(&Pose::from_json(&std::fs::read(&solution_path)?)?)
-        } else {
-            u64::MAX
-        };
-        println!("Solving {}", i);
-        for &name in &solver_names {
-            let solutions_path = solutions_base_path.join(name);
-            std::fs::create_dir_all(&solutions_path)?;
-            let solver = SOLVERS.get(name).unwrap();
-            let pose = solver.solve(&problem);
-            let dislikes = problem.dislikes(&pose);
-            let valid = problem.validate(&pose);
-            println!("  {}: dislikes = {}, valid = {}", name, dislikes, valid);
-            if valid {
-                let current_solution_path = solutions_path.join(format!("{}.solution", i));
-                std::fs::write(&current_solution_path, pose.to_json()?)?;
-                if best_dislikes > dislikes {
-                    println!(
-                        "Replacing the current best solution ({} > {})",
-                        best_dislikes, dislikes
-                    );
-                    std::fs::copy(current_solution_path, &solution_path)?;
-                    best_dislikes = dislikes;
+    (1..=count)
+        .into_par_iter()
+        .map(|i| -> Result<()> {
+            let mut stdout = String::new();
+            let problem = Problem::from_json(&std::fs::read(
+                problems_path.join(format!("{}.problem", i)),
+            )?)?;
+            let solution_path = solutions_base_path.join(format!("{}.solution", i));
+            let mut best_dislikes = if solution_path.exists() {
+                problem.dislikes(&Pose::from_json(&std::fs::read(&solution_path)?)?)
+            } else {
+                u64::MAX
+            };
+            stdout += &format!("Problem {}\n", i);
+            for &name in &solver_names {
+                let solutions_path = solutions_base_path.join(name);
+                std::fs::create_dir_all(&solutions_path)?;
+                let solver = SOLVERS.get(name).unwrap();
+                let start = std::time::Instant::now();
+                let pose = solver.solve(&problem);
+                let time_taken = std::time::Instant::now() - start;
+                let dislikes = problem.dislikes(&pose);
+                let valid = problem.validate(&pose);
+                stdout += &format!(
+                    "  {}: dislikes = {}, valid = {}, took {}.{}s\n",
+                    name,
+                    dislikes,
+                    valid,
+                    time_taken.as_secs(),
+                    time_taken.subsec_millis()
+                );
+                if valid {
+                    let current_solution_path = solutions_path.join(format!("{}.solution", i));
+                    std::fs::write(&current_solution_path, pose.to_json()?)?;
+                    if best_dislikes > dislikes {
+                        stdout += &format!(
+                            "Replacing the current best solution ({} > {})\n",
+                            best_dislikes, dislikes
+                        );
+                        std::fs::copy(current_solution_path, &solution_path)?;
+                        best_dislikes = dislikes;
+                    }
                 }
             }
-        }
-    })
+            print!("{}", stdout);
+            Ok(())
+        })
+        .collect()
 }
